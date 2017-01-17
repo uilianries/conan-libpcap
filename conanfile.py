1af0a52145e8d81e0e37359fac5eaec28ddcb98f
Uilian Ries <uilianries@gmail.com>
2017-01-17 00:44:21 -0200

    Added conanfile receipt
    - libpcap is created by conan.io

---
+++ conanfile.py
@@ -0,0 +1,22 @@
+from conans import ConanFile, CMake
+
+class PcapConan(ConanFile):
+    name = "libpcap"
+    version = "1.8"
+    url="http://github.com/uilianries/conan-libpcap"
+    settings = "os", "compiler", "build_type", "arch"
+    generators = "cmake", "txt"
+
+    def source(self):
+        self.run("git clone --branch libpcap-1.8 https://github.com/the-tcpdump-group/libpcap.git")
+
+    def build(self):
+        self.run("%s/libpcap/configure" % self.conanfile_directory)
+        self.run("make -C %s" % self.conanfile_directory)
+
+    def package(self):
+        self.copy("*.h", dst="include", src="libpcap")
+        self.copy("*.a", dst="lib")
+
+    def package_info(self):
+        self.cpp_info.libs = ["pcap"]
