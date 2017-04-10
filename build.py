6fa8f6302b7bd64821938f404d6ae06dedb1a47e
Uilian Ries <uilianries@gmail.com>
2017-04-09 23:42:08 -0300

    Add libstdc++11 for gcc build
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- build.py
+++ build.py
@@ -1,4 +1,5 @@
 """This script build Conan.io package to multiple platforms."""
+from platform import system
 from os import getenv
 from conan.packager import ConanMultiPackager
 
@@ -7,4 +8,13 @@ if __name__ == "__main__":
     builder = ConanMultiPackager()
     builder.password = getenv("CONAN_PASSWORD")
     builder.add_common_builds(shared_option_name="libpcap:shared", pure_c=True)
+    if system() == "Linux":
+        stdlibcpp11_builds = []
+        for settings, options in builder.builds:
+            settings["compiler.libcxx"] = "libstdc++"
+            if settings["compiler.version"] > "4.9":
+                _settings = dict(settings)
+                _settings["compiler.libcxx"] = "libstdc++11"
+                stdlibcpp11_builds.append([_settings, options])
+        builder.builds = builder.builds + stdlibcpp11_builds
     builder.run()
