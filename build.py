df965abef8666ee6d0dd13f82471137bd6394ec9
Uilian Ries <uilianries@gmail.com>
2017-07-15 08:56:40 -0300

    Added lite builds
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- build.py
+++ build.py
@@ -7,4 +7,11 @@ if __name__ == "__main__":
     builder = ConanMultiPackager()
     builder.password = getenv("CONAN_PASSWORD")
     builder.add_common_builds(shared_option_name="libpcap:shared", pure_c=True)
+    for settings, options, env_vars, build_requires in reversed(builder.builds):
+        false_options = dict(options)
+        false_options["enable_usb"] = False
+        false_options["enable_bluetooth"] = False
+        false_options["enable_dbus"] = False
+        false_options["enable_packet_ring"] = False
+        builder.builds.append([settings, false_options, env_vars, build_requires])
     builder.run()
